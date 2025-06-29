05ca086ef1c3344abe3caa395c6b791d8cd55560
jiin-jung <cki08543@gmail.com>
2025-06-30 02:04:40 +0900

    Add local .gitignore before pulling

---
+++ src/test/java/coffeetech/coffeetech/CoffeetechApplicationTests.java
@@ -0,0 +1,13 @@
+package coffeetech.coffeetech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeetechApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
