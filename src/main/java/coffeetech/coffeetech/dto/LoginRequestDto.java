89f58d156f8d4851dbfc7a25bb8342306408c906
jiin-jung <cki08543@gmail.com>
2025-07-01 13:25:51 +0900

    fin login

---
+++ src/main/java/coffeetech/coffeetech/dto/LoginRequestDto.java
@@ -0,0 +1,9 @@
+package coffeetech.coffeetech.dto;
+
+import lombok.Getter;
+
+@Getter
+public class LoginRequestDto {
+    private String email;
+    private String password;
+}
---
+++ src/main/java/coffeetech/coffeetech/entity/Attend.java
@@ -0,0 +1,4 @@
+package coffeetech.coffeetech.entity;
+
+public class Attend {
+}
